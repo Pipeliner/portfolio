19f304022e0e9fcb0c467dca6a3b3cc93683e130
Vadim Evard <v.e.evard@gmail.com>
2018-02-28 17:20:18 +0300

    GET->POST + submit address and keys
    to test:
    curl -s http://localhost:3000/claimName/$NAME -d address=10b3a5d8551183a00f429d165d5f480139b93706 -d key=635202ecb8337035982eb7d21269375f67652e51a9ac7f31e98e0006a1745fed

--- rest-demo/server/server.js
+++ rest-demo/server/server.js
@@ -3,9 +3,6 @@ const Web3 = require("web3");
 const Tx = require('ethereumjs-tx')
 const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io"));
 
-const account = "10b3a5d8551183a00f429d165d5f480139b93706";
-const key = new Buffer('635202ecb8337035982eb7d21269375f67652e51a9ac7f31e98e0006a1745fed', 'hex')
-
 const tokenABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_SUPPLY","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}];
 const tokenAddress = "0xd5c4210e6a53aeddcfaa1a81d621bb648e089340";
 
@@ -20,18 +17,20 @@ const gasPrice = web3.eth.gasPrice;
 const gasPriceHex = web3.utils.toHex(gasPrice);
 const gasLimitHex = web3.utils.toHex(3000000);
 
-function replyWhenTxIsReady(res, txData, to) {
-	web3.eth.getTransactionCount(account).then(txCount => {
+function replyWhenTxIsReady(res, account, txData, to) {
+	console.log(account);
+	web3.eth.getTransactionCount(account.address).then(txCount => {
 		var tra = {
 		    gasPrice: web3.utils.toHex(10e9), // 10 Gwei
 		    gasLimit: gasLimitHex,
 		    data: txData,
-		    from: account,
+		    from: account.address,
 		    to: to,
 		    nonce: txCount
 		};
 
 		var tx = new Tx(tra);
+		key = new Buffer(account.key, 'hex');
 		tx.sign(key);
 
 		var stx = tx.serialize();
@@ -42,21 +41,30 @@ function replyWhenTxIsReady(res, txData, to) {
 	});
 }
 
-
-function sendYourself(res, value) {
-	txData = token.methods.transfer("10b3a5d8551183a00f429d165d5f480139b93706", value).encodeABI();
-	replyWhenTxIsReady(res, txData, tokenAddress);
+function addressAndKey(req) {
+	return {
+		address: req.body.address,
+		key: req.body.key
+	};
 }
 
-function claimName(res, name) {
+
+function sendYourself(res, account, value) {
+	txData = token.methods.transfer("10b3a5d8551183a00f429d165d5f480139b93706", value).encodeABI();
+	replyWhenTxIsReady(res, account, txData, tokenAddress);
+}
+
+function claimName(res, account, name) {
 	txData = registry.methods.claimName(name).encodeABI();
-	replyWhenTxIsReady(res, txData, registryAddress);
+	replyWhenTxIsReady(res, account, txData, registryAddress);
 }
 
 const express = require('express');
 const app = express();
+app.use(express.urlencoded());
 
-app.get('/sendYourself/:value', (req, res) => sendYourself(res, req.params.value));
-app.get('/claimName/:name',     (req, res) => claimName(res, req.params.name));
+
+app.post('/sendYourself/:value', (req, res) => sendYourself(res, addressAndKey(req), req.params.value));
+app.post('/claimName/:name',     (req, res) => claimName(res, addressAndKey(req), req.params.name));
 
 app.listen(3000, () => console.log('Name registry REST service is running at http://localhost:3000'));
